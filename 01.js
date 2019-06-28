30aef573142f6a4cf2dad0adcca70788420fcdd0
Ryukito0116 <Ryukito.rabbit.0116@icloud.com>
2019-06-28 13:18:29 +0900

    6/28 課題

--- 01.js
+++ 01.js
@@ -1,10 +1,13 @@
 // FizzBuzz
 
-// サンプル: "1" という文字列を出力
-console.log("1");
-
-// サンプル: "FizzBuzz" という文字列を出力
-// (process.stdout.write で改行なしの出力ができる)
-process.stdout.write("Fizz");
-process.stdout.write("Buzz");
-console.log();
+for (var n=1; n<16; n++){
+    if (n%15 === 0){
+        console.log("FizzBuzz");
+    } else if (n%5 === 0){
+        console.log("Buzz");
+    } else if (n%3 === 0){
+        console.log("Fizz");
+    } else {
+        console.log(n);
+    }
+}
